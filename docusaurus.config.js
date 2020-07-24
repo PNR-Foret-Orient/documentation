3c62fd2caf657b965495c3a135b460a77d9dff09
George Moon <george@moon.org.nz>
2020-07-24 15:05:45 +1200

    URL correction

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/PNR-Foret-Orient/documentation/edit/master/website/',
+            'https://github.com/PNR-Foret-Orient/documentation/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
